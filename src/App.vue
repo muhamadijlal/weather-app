3834b50c7c7d41807a24165b081479bb90daad5c
muhamadijlal <haidarijlal027@gmail.com>
2023-07-31 21:16:07 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import WeatherWidget from "@/components/WeatherWidget.vue";
+</script>
+
+<template>
+  <div class="grid place-content-center h-screen">
+    <weather-widget />
+  </div>
+</template>
---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-slate-800 space-y-5 rounded-lg px-10 py-14">
+    <div class="flex justify-between items-center">
+      <h4 class="text-4xl text-slate-100 font-bold">
+        Delhi IN
+        <p class="text-2xl font-medium">Smoke</p>
+      </h4>
+
+      <h4 class="text-9xl">☀️</h4>
+    </div>
+
+    <div class="flex gap-5 justify-between items-center">
+      <div>
+        <h4 class="text-[10rem] font-bold text-slate-100">19°C</h4>
+      </div>
+      <div>
+        <table class="text-slate-100 font-medium text-xl w-56">
+          <tbody>
+            <tr>
+              <td colspan="2">Details</td>
+            </tr>
+            <tr>
+              <td>Feels like:</td>
+              <td class="text-right">17.9819°C</td>
+            </tr>
+            <tr>
+              <td>Humidity:</td>
+              <td class="text-right">37%</td>
+            </tr>
+            <tr>
+              <td>Pressure:</td>
+              <td class="text-right">1017hPs</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
